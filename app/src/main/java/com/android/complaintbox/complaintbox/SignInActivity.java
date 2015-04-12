38f70d2ed8c49a57e118b6fb1406d947b5c2e22c
rishabh <rishabhbaid1@gmail.com>
2015-04-12 18:17:38 +0530

    Setup in Linux and client side Sign-In code

--- app/src/main/java/com/android/complaintbox/complaintbox/SignInActivity.java
+++ app/src/main/java/com/android/complaintbox/complaintbox/SignInActivity.java
@@ -1,15 +1,22 @@
 package com.android.complaintbox.complaintbox;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.android.complaintbox.backend.myApi.MyApi;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.plus.Plus;
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
 import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
@@ -18,7 +25,18 @@ import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 import java.io.IOException;
 
 
-public class SignInActivity extends ActionBarActivity {
+public class SignInActivity extends ActionBarActivity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        View.OnClickListener  {
+
+    /* Request code used to invoke sign in user interactions. */
+    private static final int RC_SIGN_IN = 0;
+
+    /* Client used to interact with Google APIs. */
+    private GoogleApiClient mGoogleApiClient;
+
+    private boolean mIntentInProgress = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +44,72 @@ public class SignInActivity extends ActionBarActivity {
         setContentView(R.layout.activity_sign_in);
 
         new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "Manfred"));
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Plus.API)
+                .addScope(new Scope("profile"))
+                .build();
+
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+
+        if(!mIntentInProgress && result.hasResolution()) {
+            try {
+                mIntentInProgress =  true;
+                result.startResolutionForResult(this,RC_SIGN_IN);
+            } catch (IntentSender.SendIntentException e) {
+                // The intent was canceled before it was sent.  Return to the default
+                // state and attempt to connect to get an updated ConnectionResult.
+                mIntentInProgress = false;
+                mGoogleApiClient.connect();
+            }
+        }
+    }
+
+    @Override
+    public void onConnected (Bundle connectionHint) {
+        // We've resolved any connection errors.  mGoogleApiClient can be used to
+        // access Google APIs on behalf of the user.
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int responseCode, Intent intent) {
+        if(requestCode == RC_SIGN_IN) {
+            mIntentInProgress= false;
+
+            if(!mGoogleApiClient.isConnected()) {
+                mGoogleApiClient.reconnect();
+            }
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        mGoogleApiClient.connect();
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
