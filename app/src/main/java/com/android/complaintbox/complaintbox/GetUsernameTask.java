b7ed1057c00d4b3dc2d01d63ed7fd8a0a8da1e7f
rishabh <rishabhbaid1@gmail.com>
2015-04-19 19:41:26 +0530

    Token Support, many issue

---
+++ app/src/main/java/com/android/complaintbox/complaintbox/GetUsernameTask.java
@@ -0,0 +1,7 @@
+package com.android.complaintbox.complaintbox;
+
+/**
+ * Created by rishabh on 19/4/15.
+ */
+public class GetUsernameTask {
+}
---
+++ backend/src/main/java/com/android/complaintbox/backend/Checker.java
@@ -0,0 +1,7 @@
+package com.android.complaintbox.backend;
+
+/**
+ * Created by rishabh on 19/4/15.
+ */
+public class Checker {
+}
